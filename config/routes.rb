ab166c35e3aef3efe347e7c94f079166a22eb580
John Marty <JohnRmarty@gmail.com>
2015-02-23 10:06:56 -0700

    controller and views complete for users crud and courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'courses/index'
+
+  get 'users/show'
+
+  get 'users/new'
+
+  get 'users/edit'
+
+  get 'users/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
