44ed246d42073da9c1b96cfbb1c31a9377d481d5
John Marty <JohnRmarty@gmail.com>
2015-02-23 10:41:23 -0700

    complete user controller crud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,40 @@
 class UsersController < ApplicationController
-  def show
-  end
-
-  def new
-  end
-
-  def edit
-  end
+  
 
   def index
+  	@users = User.all 
   end
-end
+
+
+  def new
+  	@user = User.new
+  end
+
+   def create
+	@user = User.new(user_params)
+	if @user.save
+		flash[:notice] = "You are now a user!"
+		sign_in @user
+		redirect_to manufacturers_path
+	else
+		render :new
+	end 
+	end 
+
+  def edit
+  	@user = User.find(params[:id])
+  end
+
+   def show
+   	  @user = User.find(params[:id])
+  end
+
+  
+private
+	def user_params
+		params.require(:user).permit(:user_name, :password, :about )
+	end 
+
+
+
+end 
